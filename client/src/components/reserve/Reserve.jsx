55b4ed1084806bf68c84e9307d70a3e749581d8a
yas-chal <yasserchalal@gmail.com>
2022-10-08 10:00:02 +0100

    hejf

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -9,9 +9,9 @@ import { axiosInstance } from '../../config'
 
 
 
-const Reserve = ({setOpen, hotelId}) => {
+const Reserve = ({setOpen, roomId}) => {
     const [selectedRooms, setSelectedRooms] = useState([])
-    const {data,loading} = useFetch(`/room/${hotelId}`)
+    const {data,loading} = useFetch(`/rooms/${roomId}`)
     const {dates} = useContext(SearchContext)
     const getDatesInRange = (startDate,endDate)=>{
         const start = new Date(startDate)
@@ -27,13 +27,13 @@ const Reserve = ({setOpen, hotelId}) => {
         }
         return dates ;
     }
-    /*const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate)
+    const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate)
     const isAvailable = (roomNumber) => {
        const isFound = roomNumber.unavailableDates.some(date => 
            alldates.includes(new Date(date).getTime())
          ) ;
          return !isFound
-      }*/
+      }
     
     const handleSelect =(e) =>{
        const checked = e.target.checked
@@ -56,41 +56,32 @@ const Reserve = ({setOpen, hotelId}) => {
         
          }
      }
-      const datas = Array.from(data)
 
-     const titles = datas.map(element=> element.title)
-     const descrip = datas.map(element=> element.desc)
-     const mpe = datas.map((element)=> element.maxPeople)
-     const prix = datas.map((element)=> element.price)
-     const ronum = datas.map((element)=> element.roomNumbers)
-     const ronumnum = datas.map(roomNumber=> roomNumber.number)
-     const ronumid = datas.map(roomNumber=> roomNumber._id)
-     const ronumnu = datas.map(roomNumber=> roomNumber)
-
+     const datas = Array.from(data)
   return (
     <div className='reserve'>
         <div className="rContainer">
             <FontAwesomeIcon icon={faCircleXmark} className='rClose' onClick={()=>setOpen(false)}/>
             <span>Select your Rooms:</span>
-            
+            {datas.map((item) =>(
               <div className="rItem">
                 <div className="rItemInfo">
-                    <div className="rTitle">{titles}</div>
-                    <div className="rDesc">{descrip}</div>
-                    <div className="rMax">Max People : <b>{mpe}</b></div>
-                    <div className="rPrice">${prix}</div>
+                    <div className="rTitle">{item.title}</div>
+                    <div className="rDesc">{item.desc}</div>
+                    <div className="rMax">Max People : <b>{item.maxPeople}</b></div>
+                    <div className="rPrice">${item.price}</div>
                     </div>
-                    
+                    {item.roomNumbers?.map(roomNumber=>(
                       <div className="room">  
-                      <label>{ronumnum}</label>
-                        <input type="checkbox" value={ronumid} onChange={handleSelect} disabled={!isAvailable(ronumnu)} />
+                      <label>{roomNumber.number}</label>
+                        <input type="checkbox" value={roomNumber._id} onChange={handleSelect} disabled={!isAvailable(roomNumber)} />
                     </div>
-                  
+                    ))}
                 </div>
               
-            
+            )
 
-            
+            )}
              <button onClick={handleClick} className='rbutton'>Reserve Now</button>
         </div>
     </div>
