720e831b778704681a1c4d0f5db9627975e38fb9
yas-chal <yasserchalal@gmail.com>
2022-12-04 19:46:50 +0100

    css

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,55 +1,59 @@
-import "./navbar.css"
-import {Link, useNavigate} from 'react-router-dom'
-import { useContext } from "react"
-import { AuthContext } from "../../context/AuthContext"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-  const {user} = useContext(AuthContext)
-  const {loading, error, dispatch} = useContext(AuthContext)
-  const navigate = useNavigate()
+  const { user } = useContext(AuthContext);
+  const { loading, error, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
 
   const goToRegisterPage = () => {
-    navigate('/register')
-  }
+    navigate("/register");
+  };
   const logout = () => {
-    dispatch({type:"LOGOUT"})
-    navigate('/')
-  }
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
 
   return (
     <div className="navbar">
-        <div className="navContainer">
-          <Link to="/" style={{color:"inherit", textDecoration:"none"}}> 
-          <span className="logo"> Malou Booking </span>
-          </Link>
-           {user 
-           ? <>
-           <img src={user.img} alt="" />
-           <div className="navItems">
-                   
-                   <span> {user.username} </span>
-                   <button onClick={logout} >Logout</button>
-                   <FontAwesomeIcon className="logout" icon={faArrowRightFromBracket} />
-           </div>
-           </>
-           : (
-            <div>
-                <button onClick={goToRegisterPage} className="navButton">Register</button>
-                <button onClick={handleClick} className="navButton">Login</button>
-            </div>
-           )
-           
-           }
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo"> MalooBooking </span>
+        </Link>
+        {user ? (
+          <>
             
-        </div>
+            <div className="navItems">
+              <img src={user.img} alt="" />
+              <span> {user.username} </span>
+              <button onClick={logout}>Logout</button>
+              <FontAwesomeIcon
+                className="logout"
+                icon={faArrowRightFromBracket}
+              />
+            </div>
+          </>
+        ) : (
+          <div>
+            <button onClick={goToRegisterPage} className="navButton">
+              Sign up
+            </button>
+            <button onClick={handleClick} className="navButton">
+              Login
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -11,10 +11,9 @@ const Register = () => {
   const [file, setFile] = useState("");
   const [info, setInfo] = useState({});
 
+  const { loading, error, dispatch } = useContext(AuthContext);
 
-  const {loading, error, dispatch} = useContext(AuthContext)
-
- const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
@@ -38,15 +37,14 @@ const Register = () => {
         img: url,
       };
       await axiosInstance.post("/auth/register", newUser);
-      dispatch({type : "LOGIN_START"})
-      try{
-         const res = await axiosInstance.post('/auth/login',info)
-         dispatch({type:"LOGIN_SUCCESS", payload : res.data.details})
-         navigate('/')
-      }catch(err){
-          dispatch({type:"LOGIN_FAILURE", payload : err.response.data})
+      dispatch({ type: "LOGIN_START" });
+      try {
+        const res = await axiosInstance.post("/auth/login", info);
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        navigate("/");
+      } catch (err) {
+        dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
       }
-
     } catch (err) {
       console.log(err);
     }
@@ -54,88 +52,88 @@ const Register = () => {
 
   return (
     <>
-    <div className="bg"></div>
-    <div className="bg bg2"></div>
-    <div className="bg bg3"></div>
-    <div className="login">
-      <div className="login_container">
-        <div className="register">
-          <h1>Register</h1>
-          <div className="rI">
-            <FontAwesomeIcon icon={faUser} className="rI_icon" />
-            <input
-              type="text"
-              onChange={handleChange}
-              id="username"
-              placeholder="User Name"
-            />
-          </div>
-          <div className="rI">
-            <FontAwesomeIcon icon={faEnvelope} className="rI_icon" />
-            <input
-              type="text"
-              onChange={handleChange}
-              id="email"
-              placeholder="E-mail"
-            />
-          </div>
-          <div className="rI">
-            <FontAwesomeIcon icon={faLock} className="rI_icon" />
-            <input
-              id="password"
-              onChange={handleChange}
-              type="password"
-              className="password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="rI">
-            <FontAwesomeIcon icon={faEarthEurope} className="rI_icon" />
-            <input
-              type="text"
-              onChange={handleChange}
-              id="country"
-              placeholder="Country"
-            />
-          </div>
-          <div className="rI">
-            <FontAwesomeIcon icon={faMapLocationDot} className="rI_icon" />
-            <input
-              type="text"
-              onChange={handleChange}
-              id="city"
-              placeholder="City"
-            />
-          </div>
-          <div className="rI">
-            <FontAwesomeIcon icon={faPhone} className="rI_icon" />
-            <input
-              type="text"
-              onChange={handleChange}
-              id="phone"
-              placeholder="Phone Number"
-            />
-          </div>
-          <div className="formInput">
-            <label htmlFor="file">
-              Photo : <FontAwesomeIcon icon={faFileImage} className="icon" />
-            </label>
-            <input
-              type="file"
-              id="file"
-              onChange={(e) => setFile(e.target.files[0])}
-              className="file"
-            />
-          </div>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+      <div className="login">
+        <div className="login_container">
+          <div className="register">
+            <h1>Register</h1>
+            <div className="rI">
+              <FontAwesomeIcon icon={faUser} className="rI_icon" />
+              <input
+                type="text"
+                onChange={handleChange}
+                id="username"
+                placeholder="User Name"
+              />
+            </div>
+            <div className="rI">
+              <FontAwesomeIcon icon={faEnvelope} className="rI_icon" />
+              <input
+                type="text"
+                onChange={handleChange}
+                id="email"
+                placeholder="E-mail"
+              />
+            </div>
+            <div className="rI">
+              <FontAwesomeIcon icon={faLock} className="rI_icon" />
+              <input
+                id="password"
+                onChange={handleChange}
+                type="password"
+                className="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="rI">
+              <FontAwesomeIcon icon={faEarthEurope} className="rI_icon" />
+              <input
+                type="text"
+                onChange={handleChange}
+                id="country"
+                placeholder="Country"
+              />
+            </div>
+            <div className="rI">
+              <FontAwesomeIcon icon={faMapLocationDot} className="rI_icon" />
+              <input
+                type="text"
+                onChange={handleChange}
+                id="city"
+                placeholder="City"
+              />
+            </div>
+            <div className="rI">
+              <FontAwesomeIcon icon={faPhone} className="rI_icon" />
+              <input
+                type="text"
+                onChange={handleChange}
+                id="phone"
+                placeholder="Phone Number"
+              />
+            </div>
+            <div className="formInput">
+              <label htmlFor="file">
+                Photo : <FontAwesomeIcon icon={faFileImage} className="icon" />
+              </label>
+              <input
+                type="file"
+                id="file"
+                onChange={(e) => setFile(e.target.files[0])}
+                className="file"
+              />
+            </div>
 
-          <button onClick={handleClick}>Create account</button>
-          <Link to={"/login"}>
-            <p>Back To Login</p>{" "}
-          </Link>
+            <button onClick={handleClick}>Create account</button>
+            <Link to={"/login"}>
+              <p>Back to login</p>{" "}
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
